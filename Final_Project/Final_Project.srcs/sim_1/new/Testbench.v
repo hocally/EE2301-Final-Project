04324eedfc7b08478725d1e124ee2325c3fa30d6
phildaniels22 <38110466+phildaniels22@users.noreply.github.com>
2018-04-08 17:26:19 -0500

    Testbench Start

---
+++ Final_Project/Final_Project.srcs/sim_1/new/Testbench.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/08/2018 04:41:53 PM
+// Design Name: 
+// Module Name: Testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ________(
+    
+    );
+    
+reg [7:0] Mplier;
+reg [7:0] Mcand;
+wire [15:0] Product;
+    
+initial 
+
+    begin
+    Mplier = 0; Mcand = 8'b10101110;
+    #50
+    Mplier = 1; Mcand = 8'b10101110;
+    #50
+    Mplier = 8'b10101110; Mplier = 1;
+    #50
+    Mplier = 8; Mcand = 80;
+    #50
+    Mplier = 100; Mcand = 15;
+    end 
+    
+endmodule

4e8ebfe8add7d6a95f5cf0f423dc183702d0a504
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2018-04-10 14:27:35 -0500

    Started FSM implementation

--- Final_Project/Final_Project.srcs/sources_1/new/multiplier_1_0.v
+++ Final_Project/Final_Project.srcs/sources_1/new/multiplier_1_0.v
@@ -20,10 +20,68 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module multiplier_1_0(a, b, clk, out);
-input [7:0] a;
-input [7:0] b;
-input clk;
-output reg [15:0] out;
+module multiplier_1_0(a, b, clk, out, start);
+    input [7:0] a;
+    input [7:0] b;
+    input clk;
+    input start;
+    output reg [15:0] out;
+    reg [2:0] State=4'b0000, NextState=4'b0000;
+
+//state register
+    always @(posedge clk)
+        State <= NextState;
+
+//state stuff
+    always @(State)
+    case(State)
+        0:if(Start) begin
+            NextState = 1;
+            end
+        else begin
+            NextState = 0;
+            end
+        1:if(a[0] == 1'b0) begin
+            NextState = 3;
+            end
+          else begin
+            NextState = 2;
+            end
+        2:begin
+            //addition code?
+            NextState = 3;
+            end
+        1:if(a[0] == 1'b0) begin
+            NextState = 3;
+            end
+          else begin
+            NextState = 2;
+            end
+        2:begin
+           //addition code?
+            NextState = 3;
+            end
+        1:if(a[0] == 1'b0) begin
+            NextState = 3;
+            end
+          else begin
+            NextState = 2;
+            end
+        2:begin
+          //addition code?
+            NextState = 3;
+            end
+        1:if(a[0] == 1'b0) begin
+            NextState = 3;
+            end
+          else begin
+            NextState = 2;
+            end
+        2:begin
+         //addition code?
+            NextState = 3;
+            end
+                                                        
+        
 
 endmodule
